82b584b4caed8dcf3d782205c496ca5cc3af823c
bicklil <lemanjc@gmail.com>
2017-03-24 09:37:21 +0100

    ajout de DROP THE BASSé

---
+++ DebProj/clean_db.sql
@@ -0,0 +1,14 @@
+DROP TABLE Utilisateur;
+DROP TABLE OffreRectrument;
+DROP TABLE Rang;
+DROP TABLE Message;
+DROP TABLE Sujet;
+DROP TABLE Categorie;
+DROP TABLE Support;
+DROP TABLE Statistiques;
+DROP TABLE MsgPrive;
+DROP TABLE Programmation;
+DROP TABLE UtilisateurProgrammation;
+DROP TABLE OffreRecrutementProgrammation;
+
+DROP DOMAIN Status;
