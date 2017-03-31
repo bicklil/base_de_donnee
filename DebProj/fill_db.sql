407b4738bcfb16cec234d9e5351e08281ac87bf9
bicklil <lemanjc@gmail.com>
2017-03-31 09:09:35 +0200

    Ajout dinsert

--- DebProj/fill_db.sql
+++ DebProj/fill_db.sql
@@ -1,6 +1,7 @@
 insert into rang values ('Nooblard',0),('Bronzos',2),('Kevin',5);
 insert into utilisateur values ('kiki','lol@lol','2003-07-01','M','toulon','kikou',0,0,'2003-07-01','Lambda','Kevin');
-insert into section values ('section1',0,null);
-insert into categorie values ('categorie1',0,'section1');
+insert into section values ('section1',0,null),('section2',0,null);
+insert into categorie values ('categorie1',0,'section1'),('categorie2',0,'section1'),
+                             ('categorie3',0,'section2'),('categorie4',0,'section2');
 insert into sujet values (0,'hihi','2003-05-05',0,'categorie1','kiki');
 insert into Message values(0,'2003-05-05','hihi',0,'kiki',0);
