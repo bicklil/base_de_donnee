c5b7238ff95cce16e3f8f8539634d877277ff813
bicklil <lemanjc@gmail.com>
2017-04-07 11:55:11 +0200

    Ajout modif default fill

--- DebProj/fill_db.sql
+++ DebProj/fill_db.sql
@@ -4,7 +4,7 @@ insert into utilisateur values ('kou','lol@loazeazel','2003-07-01','M','toulon',
 insert into section values ('section1',0,null),('section2',0,null);
 insert into categorie values ('categorie1',0,'section1'),('categorie2',0,'section1'),
                              ('categorie3',0,'section2'),('categorie4',0,'section2');
-insert into sujet values (0,'hihi','2003-05-05 00:00:00',0,'categorie1','kiki');
+insert into sujet values (DEFAULT,'hihi','2003-05-05 00:00:00',0,'categorie1','kiki');
 insert into Message values(DEFAULT,'2003-05-05 00:01:00','hihi',0,'kiki',0);
 insert into Message values(DEFAULT,'2003-05-05 01:01:00','hihi',0,'kiki',0);
 insert into Message values(DEFAULT,'2003-05-05 02:01:00','hihi',0,'kiki',0);
@@ -19,6 +19,6 @@ insert into Message values(DEFAULT,'2003-05-05 10:01:00','hihi',0,'kiki',0);
 insert into Message values(DEFAULT,'2003-05-05 11:01:00','hihi',0,'kiki',0);
 
 insert into Message values(DEFAULT,'2003-05-05 11:01:00','hihi',0,'kiki',0);
-insert into OffreRecrutement values (1, '2017-03-30', '2017-04-15', ' ', 'CDI', 'rct dj cm lvl 150 min passe vite', 'kou');
-insert into OffreRecrutement values (2, '2017-03-15', '2017-03-30', ' ', 'CDD', 'rct de sexe f et m pour tue le karalamoure', 'kou');
+insert into OffreRecrutement values (DEFAULT, '2017-03-30', '2017-04-15', ' ', 'CDI', 'rct dj cm lvl 150 min passe vite', 'kou');
+insert into OffreRecrutement values (DEFAULT, '2017-03-15', '2017-03-30', ' ', 'CDD', 'rct de sexe f et m pour tue le karalamoure', 'kou');
 insert into UtilisateurOffreRecrutement values (1,'kiki'), (2,'kiki');
